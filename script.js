e0ea99a359dc192f3cdc3d16acbecc4449ce0a75
WHOVION <eszrajs@gmail.com>
2023-04-06 17:11:54 -0700

    studying and taking notes on what is happening

--- script.js
+++ script.js
@@ -14,12 +14,11 @@ let realTimeScreenValue = []
 // To Clear
 
 clearbtn.addEventListener("click", () => {
-
     realTimeScreenValue = [''];
     answerScreen.innerHTML = 0;
     currentInput.className = 'currentInput'
     answerScreen.className = 'answerScreen';
-    answerScreen.style.color = " rgba(150, 150, 150, 0.87)";
+    answerScreen.style.color = 'green'
 })
 
 // Get value of any button clicked and display to the screen
@@ -54,7 +53,7 @@ buttons.forEach((btn) => {
         if (btn.id.match('evaluate')) {
             currentInput.className = 'answerScreen';
             answerScreen.className = 'currentInput';
-            answerScreen.style.color = "white";
+            // answerScreen.style.color = "white";
         }
 
         // To prevent undefined error in screen
